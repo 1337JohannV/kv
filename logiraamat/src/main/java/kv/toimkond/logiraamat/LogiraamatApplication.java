6c5f32ff24612697ff6681ab2b7cc037863f1570
Johann Veispak <johann.veispak@gmail.com>
2021-01-09 17:13:25 +0200

    init

---
+++ logiraamat/src/main/java/kv/toimkond/logiraamat/LogiraamatApplication.java
@@ -0,0 +1,13 @@
+package kv.toimkond.logiraamat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogiraamatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogiraamatApplication.class, args);
+    }
+
+}
---
+++ logiraamat/src/test/java/kv/toimkond/logiraamat/LogiraamatApplicationTests.java
@@ -0,0 +1,13 @@
+package kv.toimkond.logiraamat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogiraamatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
